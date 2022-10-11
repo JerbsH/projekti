e619fb43172ed8d6d8b909c9ebfdc87ed1d27b6d
Jere <hippelainen.jere@gmail.com>
2022-10-11 20:16:03 +0300

    formatting

--- js/scroll.js
+++ js/scroll.js
@@ -1,29 +1,30 @@
 //Tehnyt: Jere, muokattu 5.10.2022
 //Netistä löydettyä ideaa hieman sovellettu
-var video = document.getElementById('video');
+var video = document.getElementById("video");
 
 function fadeOutOnScroll(element) {
-  if (!element) {
-    return;
-  }
-  
-  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
-  var elementHeight = element.offsetHeight;
-  var scrollTop = document.documentElement.scrollTop;
-  
-  var opacity = 1;
-  
-  if (scrollTop > distanceToTop) {
-    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
-  }
-  
-  if (opacity >= 0) {
-    element.style.opacity = opacity;
-  }
+	if (!element) {
+		return;
+	}
+
+	var distanceToTop =
+		window.pageYOffset + element.getBoundingClientRect().top;
+	var elementHeight = element.offsetHeight;
+	var scrollTop = document.documentElement.scrollTop;
+
+	var opacity = 1;
+
+	if (scrollTop > distanceToTop) {
+		opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+	}
+
+	if (opacity >= 0) {
+		element.style.opacity = opacity;
+	}
 }
 
 function scrollHandler() {
-  fadeOutOnScroll(video);
+	fadeOutOnScroll(video);
 }
 
-window.addEventListener('scroll', scrollHandler);
+window.addEventListener("scroll", scrollHandler);
--- js/tietoja.js
+++ js/tietoja.js
@@ -1,28 +1,29 @@
-'use strict';
+"use strict";
 
-const sectionElem = document.querySelector('section');
+const sectionElem = document.querySelector("section");
 
-let articleElem = document.createElement('article');
+let articleElem = document.createElement("article");
 sectionElem.appendChild(articleElem);
 
-let h2Elem = document.createElement('h2');
+let h2Elem = document.createElement("h2");
 articleElem.appendChild(h2Elem);
-h2Elem.innerText = 'Yleistä tietoa Helsingin koirapuistoista';
+h2Elem.innerText = "Yleistä tietoa Helsingin koirapuistoista";
 
-let pElem = document.createElement('p');
-pElem.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum ultricies placerat. Nunc ultrices quam et viverra sodales. Nullam est ipsum, elementum a tempor in, gravida sit amet ipsum. Mauris dignissim mattis tincidunt. Duis ornare dui non rutrum auctor. Phasellus quis accumsan lectus, ac tristique nibh. Ut scelerisque fringilla felis ut facilisis. Sed nec velit euismod, mattis sapien a, dignissim ante. Nunc ac ligula sit amet purus consectetur pulvinar. In hac habitasse platea dictumst. Aliquam ex lorem, volutpat eu lacus nec, finibus elementum turpis. In pretium, nisl id auctor rutrum, nisi justo aliquet dui, quis fringilla lacus leo ut felis. Sed vel quam ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam quis laoreet erat. Curabitur facilisis purus eu accumsan vestibulum. Morbi lorem metus, gravida ac rhoncus suscipit, aliquam at est. Aenean consequat sit amet arcu ac lacinia. Donec luctus sem vitae nisi facilisis, a commodo magna interdum. Nam ornare consequat mi ut rutrum. In laoreet libero ac tellus malesuada sodales. Ut nec ante ligula. Integer porttitor facilisis enim non efficitur. Quisque id sapien ut dui convallis efficitur. Pellentesque commodo vitae sem eget scelerisque. Morbi ullamcorper diam eu nibh cursus cursus. Maecenas euismod iaculis."
+let pElem = document.createElement("p");
+pElem.innerText =
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum ultricies placerat. Nunc ultrices quam et viverra sodales. Nullam est ipsum, elementum a tempor in, gravida sit amet ipsum. Mauris dignissim mattis tincidunt. Duis ornare dui non rutrum auctor. Phasellus quis accumsan lectus, ac tristique nibh. Ut scelerisque fringilla felis ut facilisis. Sed nec velit euismod, mattis sapien a, dignissim ante. Nunc ac ligula sit amet purus consectetur pulvinar. In hac habitasse platea dictumst. Aliquam ex lorem, volutpat eu lacus nec, finibus elementum turpis. In pretium, nisl id auctor rutrum, nisi justo aliquet dui, quis fringilla lacus leo ut felis. Sed vel quam ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam quis laoreet erat. Curabitur facilisis purus eu accumsan vestibulum. Morbi lorem metus, gravida ac rhoncus suscipit, aliquam at est. Aenean consequat sit amet arcu ac lacinia. Donec luctus sem vitae nisi facilisis, a commodo magna interdum. Nam ornare consequat mi ut rutrum. In laoreet libero ac tellus malesuada sodales. Ut nec ante ligula. Integer porttitor facilisis enim non efficitur. Quisque id sapien ut dui convallis efficitur. Pellentesque commodo vitae sem eget scelerisque. Morbi ullamcorper diam eu nibh cursus cursus. Maecenas euismod iaculis.";
 articleElem.appendChild(pElem);
 
-let articleElem1 = document.createElement('article');
+let articleElem1 = document.createElement("article");
 sectionElem.appendChild(articleElem1);
 
-let h2Elem1 = document.createElement('h2');
+let h2Elem1 = document.createElement("h2");
 articleElem1.appendChild(h2Elem1);
-h2Elem1.innerText = 'Lisää tarkempia tietoja';
+h2Elem1.innerText = "Lisää tarkempia tietoja";
 
-let pElem1 = document.createElement('p');
+let pElem1 = document.createElement("p");
 articleElem1.appendChild(pElem1);
 
-pElem1.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum ultricies placerat. Nunc ultrices quam et viverra sodales. Nullam est ipsum, elementum a tempor in, gravida sit amet ipsum. Mauris dignissim mattis tincidunt. Duis ornare dui non rutrum auctor. Phasellus quis accumsan lectus, ac tristique nibh. Ut scelerisque fringilla felis ut facilisis. Sed nec velit euismod, mattis sapien a, dignissim ante. Nunc ac ligula sit amet purus consectetur pulvinar. In hac habitasse platea dictumst. Aliquam ex lorem, volutpat eu lacus nec, finibus elementum turpis. In pretium, nisl id auctor rutrum, nisi justo aliquet dui, quis fringilla lacus leo ut felis. Sed vel quam ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam quis laoreet erat. Curabitur facilisis purus eu accumsan vestibulum. Morbi lorem metus, gravida ac rhoncus suscipit, aliquam at est. Aenean consequat sit amet arcu ac lacinia. Donec luctus sem vitae nisi facilisis, a commodo magna interdum. Nam ornare consequat mi ut rutrum. In laoreet libero ac tellus malesuada sodales. Ut nec ante ligula. Integer porttitor facilisis enim non efficitur. Quisque id sapien ut dui convallis efficitur. Pellentesque commodo vitae sem eget scelerisque. Morbi ullamcorper diam eu nibh cursus cursus. Maecenas euismod iaculis."
+pElem1.innerText =
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum ultricies placerat. Nunc ultrices quam et viverra sodales. Nullam est ipsum, elementum a tempor in, gravida sit amet ipsum. Mauris dignissim mattis tincidunt. Duis ornare dui non rutrum auctor. Phasellus quis accumsan lectus, ac tristique nibh. Ut scelerisque fringilla felis ut facilisis. Sed nec velit euismod, mattis sapien a, dignissim ante. Nunc ac ligula sit amet purus consectetur pulvinar. In hac habitasse platea dictumst. Aliquam ex lorem, volutpat eu lacus nec, finibus elementum turpis. In pretium, nisl id auctor rutrum, nisi justo aliquet dui, quis fringilla lacus leo ut felis. Sed vel quam ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nullam quis laoreet erat. Curabitur facilisis purus eu accumsan vestibulum. Morbi lorem metus, gravida ac rhoncus suscipit, aliquam at est. Aenean consequat sit amet arcu ac lacinia. Donec luctus sem vitae nisi facilisis, a commodo magna interdum. Nam ornare consequat mi ut rutrum. In laoreet libero ac tellus malesuada sodales. Ut nec ante ligula. Integer porttitor facilisis enim non efficitur. Quisque id sapien ut dui convallis efficitur. Pellentesque commodo vitae sem eget scelerisque. Morbi ullamcorper diam eu nibh cursus cursus. Maecenas euismod iaculis.";
 articleElem1.appendChild(pElem1);
-
